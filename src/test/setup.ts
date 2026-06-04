d64e140081e397511ff2e4f5a2addc9cf91c1fb8
Marlon Xavier Delgado Ruiz <marlonxavi6@gmail.com>
2026-06-04 18:25:28 -0500

    chore: polyfills jsdom para componentes Radix en pruebas

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -2,6 +2,20 @@ import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
 import { afterEach, vi } from 'vitest';
 
+// Radix UI (Select, etc.) requiere APIs de pointer en jsdom
+if (!HTMLElement.prototype.hasPointerCapture) {
+  HTMLElement.prototype.hasPointerCapture = () => false;
+}
+if (!HTMLElement.prototype.setPointerCapture) {
+  HTMLElement.prototype.setPointerCapture = () => {};
+}
+if (!HTMLElement.prototype.releasePointerCapture) {
+  HTMLElement.prototype.releasePointerCapture = () => {};
+}
+if (!Element.prototype.scrollIntoView) {
+  Element.prototype.scrollIntoView = () => {};
+}
+
 afterEach(() => {
   cleanup();
   localStorage.clear();
